b7c241dd9d66a3cf2f35c92f98b9da64319434b1
robd36 <robdrost1@outlook.com>
2023-12-07 11:19:03 +0100

    Calculate angle, and draw line

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,9 @@
 
 int xLocation = 0;
 int yLocation = 0;
+bool characterMovable = true;
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
-
 
 void draw_pixel()
 {
@@ -34,22 +34,104 @@ void erase_pixel()
     tft.fillCircle(xLocation, yLocation, 5, ILI9341_MAGENTA);
 }
 
-void displayCharacter(int x, int y) {
-    tft.fillCircle(x + 25, y - 20, 10, COLOR_SKIN); //face
-    tft.drawPixel(x + 22, y - 23, ILI9341_BLACK); //eyes
+void displayCharacter(int x, int y)
+{
+    tft.fillCircle(x + 25, y - 20, 10, COLOR_SKIN); // face
+    tft.drawPixel(x + 22, y - 23, ILI9341_BLACK);   // eyes
     tft.drawPixel(x + 28, y - 23, ILI9341_BLACK);
-    tft.drawFastVLine(x + 26, y - 34, 5, ILI9341_BLACK); //hair
-    tft.drawFastVLine(x + 22, y - 32, 3, ILI9341_BLACK); 
-    tft.drawFastVLine(x + 28, y - 32, 3, ILI9341_BLACK); 
-    tft.fillRect(x + 5, y - 10, 40, 10, COLOR_BODY); //shirt
+    tft.drawFastVLine(x + 26, y - 32, 3, ILI9341_BLACK); // hair
+    tft.drawFastVLine(x + 22, y - 32, 3, ILI9341_BLACK);
+    tft.drawFastVLine(x + 28, y - 32, 3, ILI9341_BLACK);
+    tft.fillRect(x + 5, y - 10, 40, 10, COLOR_BODY); // shirt
     tft.fillRect(x + 10, y - 13, 30, 3, COLOR_BODY);
-    tft.fillRect(x, y, 50, 20, COLOR_MINECART); //minecart
+    tft.fillRect(x, y, 50, 20, COLOR_MINECART); // minecart
     tft.fillRect(x + 5, y + 5, 40, 10, COLOR_BROWN);
     tft.fillRect(x + 5, y + 9, 40, 2, COLOR_MINECART);
-    tft.fillCircle(x + 10, y + 20, 4, COLOR_WHEELS); //wheels
-    tft.fillCircle(x + 40, y + 20, 4, COLOR_WHEELS); 
-    tft.fillCircle(x + 10, y, 5, COLOR_SKIN); //hands
+    tft.fillCircle(x + 10, y + 20, 4, COLOR_WHEELS); // wheels
+    tft.fillCircle(x + 40, y + 20, 4, COLOR_WHEELS);
+    tft.fillCircle(x + 10, y, 5, COLOR_SKIN); // hands
     tft.fillCircle(x + 40, y, 5, COLOR_SKIN);
+}
+
+void resetSkyRight()
+{
+    tft.fillRect(xLocation + 50, 23, 10, 57, COLOR_BACKGROUND); // right side
+    tft.fillRect(xLocation + 30, 23, 25, 37, COLOR_BACKGROUND); // shoulder and face
+    tft.fillRect(xLocation, 75, 50, 5, COLOR_BACKGROUND);       // underneath minecart
+}
+
+void resetSkyLeft()
+{
+    tft.fillRect(xLocation - 10, 23, 10, 57, COLOR_BACKGROUND); // left side
+    tft.fillRect(xLocation, 23, 25, 37, COLOR_BACKGROUND);      // shoulder and face
+    tft.fillRect(xLocation, 75, 50, 5, COLOR_BACKGROUND);       // underneath minecart
+}
+
+void drawHookIdle()
+{
+    int xOrigin = xLocation + 25;
+    int yOrigin = 80;
+    // tft.drawLine(xOrigin, yOrigin, xDestination, yDestination, ILI9341_BLACK);
+
+    // Calculate the radius of the circle
+    int radius = 15;
+
+    // Calculate the angle step to draw points on the circle
+    float angleStep = 0.05; // Change this to adjust the spacing of points on the circle
+
+    for (float angle = 0; angle < 1 * PI; angle += angleStep)
+    {
+        Nunchuk.getState(NUNCHUK_ADDRESS);
+        if (Nunchuk.state.z_button == 1)
+        {
+            // Calculate the coordinates on the circle using polar coordinates
+            int xCircle = xOrigin + (int)(400 * cos(angle));
+            int yCircle = yOrigin + (int)(400 * sin(angle));
+
+            // Draw lines from origin to points on the circle
+            tft.drawLine(xOrigin, yOrigin, xCircle, yCircle, ILI9341_BLACK);
+            break;
+        }
+        else
+        {
+            // Calculate the coordinates on the circle using polar coordinates
+            int xCircle = xOrigin + (int)(radius * cos(angle));
+            int yCircle = yOrigin + (int)(radius * sin(angle));
+
+            // Draw lines from origin to points on the circle
+            tft.drawLine(xOrigin, yOrigin, xCircle, yCircle, ILI9341_BLACK);
+            _delay_ms(20);
+            tft.fillRect(xLocation, 80, 50, 25, COLOR_BROWN);
+        }
+    }
+
+    for (float angle = PI; angle > 0; angle -= angleStep)
+    {
+        Nunchuk.getState(NUNCHUK_ADDRESS);
+        if (Nunchuk.state.z_button == 1)
+        {
+            // Calculate the coordinates on the circle using polar coordinates
+            int xCircle = xOrigin + (int)(400 * cos(angle));
+            int yCircle = yOrigin + (int)(400 * sin(angle));
+
+            // Draw lines from origin to points on the circle
+            tft.drawLine(xOrigin, yOrigin, xCircle, yCircle, ILI9341_BLACK);
+            break;
+        }
+        else
+        {
+            // Calculate the coordinates on the circle using polar coordinates
+            int xCircle = xOrigin + (int)(radius * cos(angle));
+            int yCircle = yOrigin + (int)(radius * sin(angle));
+
+            // Draw lines from origin to points on the circle
+            tft.drawLine(xOrigin, yOrigin, xCircle, yCircle, ILI9341_BLACK);
+            _delay_ms(20);
+            tft.fillRect(xLocation, 80, 50, 25, COLOR_BROWN);
+        }
+    }
+
+    characterMovable = true;
 }
 
 int main(void)
@@ -75,9 +157,7 @@ int main(void)
     tft.fillRect(200, 200, 10, 10, COLOR_GOLD);
     tft.fillRect(180, 150, 10, 10, COLOR_ROCK);
 
-    displayCharacter(100, 55);
-
-
+    displayCharacter(xLocation, 55);
 
     // use Serial for printing nunchuk data
     Serial.begin(BAUDRATE);
@@ -85,34 +165,34 @@ int main(void)
     // join I2C bus as master
     Wire.begin();
 
-
-
     // Eindeloze lus
     while (1)
     {
-         if (!Nunchuk.getState(NUNCHUK_ADDRESS))
-		return (false);
-
-        erase_pixel();
+        if (!Nunchuk.getState(NUNCHUK_ADDRESS))
+            return (false);
 
         int intValueX = static_cast<int>(Nunchuk.state.joy_x_axis);
         int intValueY = static_cast<int>(Nunchuk.state.joy_y_axis);
 
-        if(intValueX < 128 && xLocation > 0) {
-            xLocation--;
+        // move character left and right
+        if ((intValueX < 128 && xLocation > 0) && characterMovable)
+        {
+            xLocation -= 5;
+            resetSkyRight();
+            displayCharacter(xLocation, 55);
         }
-        if(intValueX > 128 && xLocation < 320) {
-            xLocation++;
+        if ((intValueX > 128 && xLocation < 270) && characterMovable)
+        {
+            xLocation += 5;
+            resetSkyLeft();
+            displayCharacter(xLocation, 55);
         }
-        if(intValueY > 128 && yLocation > 0) {
-            yLocation--;
+
+        if (Nunchuk.state.c_button == 1)
+        {
+            characterMovable = false;
+            drawHookIdle();
         }
-        if(intValueY < 128 && yLocation < 240) {
-            yLocation++;
-        }
-       
-        // _delay_ms(50);
-        draw_pixel();
     }
 
     return 0;
