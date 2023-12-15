f92982b5eac577780e64ad37b9fe575d48ed3483
ENIGMA <34719149+enigma999@users.noreply.github.com>
2023-12-15 11:18:08 +0100

    added comments

--- lib/Time/time.cpp
+++ lib/Time/time.cpp
@@ -10,7 +10,8 @@ time::time()
 void time::initateTimer2()
 {
     TCCR2A |= (1 << WGM21) | (0 << WGM20);
-    TCCR2B |= (0 << WGM22) | (1 << CS22) | (1 << CS21) | (1 << CS20);
+    TCCR2B |= (0 << WGM22);
+    TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
     TIMSK2 |= (1 << OCIE2A);
     OCR2A = 7;
     TCNT2 = 0;
