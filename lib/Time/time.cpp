7d605ef11c192188d98029dc291df89946990838
ENIGMA <34719149+enigma999@users.noreply.github.com>
2023-12-15 11:21:27 +0100

    added comments and removed double sei()

--- lib/Time/time.cpp
+++ lib/Time/time.cpp
@@ -9,15 +9,16 @@ time::time()
 
 void time::initateTimer2()
 {
+    // mode of operation
     TCCR2A |= (1 << WGM21) | (0 << WGM20);
     TCCR2B |= (0 << WGM22);
+    // prescaler
     TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
     TIMSK2 |= (1 << OCIE2A);
+    //set interupt
     OCR2A = 7;
+    //set initial value timer
     TCNT2 = 0;
-
-    DDRB |= (1 << PINB5);
-    sei();
 }
 
 long time::getSecond()
