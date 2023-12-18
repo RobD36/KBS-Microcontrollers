c094d4def757b44477a321c2c75f7e3604ad8112
ENIGMA <34719149+enigma999@users.noreply.github.com>
2023-12-18 14:33:29 +0100

    added the notes in comments

--- lib/Buzzer/buzzer.cpp
+++ lib/Buzzer/buzzer.cpp
@@ -1,7 +1,22 @@
 #include "buzzer.h"
 #include "arduino.h"
-//G2 - G3
-int notes[] = {120, 129, 138, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262};
+/*G2 - G3
+120 Hz: G2
+129 Hz: G#2 or Ab2
+138 Hz: A2
+147 Hz: A#2 or Bb2
+156 Hz: B2
+165 Hz: C3
+175 Hz: C#3 or Db3
+185 Hz: D3
+196 Hz: D#3 or Eb3
+208 Hz: E3
+220 Hz: F3
+233 Hz: F#3 or Gb3
+247 Hz: G3
+262 Hz: G#3 or Ab3
+*/ 
+int notes[] = {262, 247, 233, 220, 208, 196, 185, 175, 165, 156, 147, 138, 129, 120, 0};
 
 ISR(TIMER2_COMPA_vect){}
 
@@ -9,7 +24,7 @@ buzzer::buzzer(){
     timer2_init();
 }
 
-void timer2_init(void)
+void buzzer::timer2_init(void)
 {
     // Set PD3 as output
     DDRD |= (1 << DDD3);
